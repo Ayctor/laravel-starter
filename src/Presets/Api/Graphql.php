282b977e4208faef70250742563af689d5598c77
Axel Charpentier <axelc@ayctor.com>
2020-10-08 09:56:55 +0200

    add graphql preset

--- src/Presets/Api/Graphql.php
+++ src/Presets/Api/Graphql.php
@@ -16,6 +16,33 @@ class Graphql extends Preset
         static::info('');
         static::info('Starts the graphql preset installation');
 
-        // TODO: add the necessary things
+        static::info('Run composer and artisan commands');
+        shell_exec('composer require nuwave/lighthouse -q');
+        shell_exec('composer require mll-lab/laravel-graphql-playground -q');
+        shell_exec('php artisan vendor:publish --tag=lighthouse-schema -q');
+        shell_exec('php artisan lighthouse:ide-helper -q');
+        shell_exec('php artisan vendor:publish --tag=lighthouse-config -q');
+        shell_exec('php artisan vendor:publish --tag=graphql-playground-config -q');
+
+        static::info('Change the graphql path in the configuration file');
+        static::replaceConfigValue(
+            config_path('lighthouse.php'),
+            "'uri' => '/graphql'",
+            "'uri' => '/api/graphql'"
+        );
+
+        static::info('Change the graphql playground path in the configuration file');
+        static::replaceConfigValue(
+            config_path('graphql-playground.php'),
+            "'uri' => '/graphql-playground'",
+            "'uri' => '/tools/graphql'"
+        );
+
+        static::info('Change the graphql playground endpoint path in the configuration file');
+        static::replaceConfigValue(
+            config_path('graphql-playground.php'),
+            "'endpoint' => '/graphql'",
+            "'endpoint' => '/api/graphql'"
+        );
     }
 }
